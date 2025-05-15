b18e6fe717b4d46fa1e832c3df9c00173d5f1c7b
Dziyana Bortnik <dziyana.bortnik@student.ehu.lt>
2025-05-15 17:56:02 +0300

    Fixed firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKW-d7fIGIvihQMVyjzzU4EkUoI7n5zl4",
-  authDomain: "auth-task-d910a.firebaseapp.com",
-  projectId: "auth-task-d910a",
-  storageBucket: "auth-task-d910a.firebasestorage.app",
-  messagingSenderId: "766460161049",
-  appId: "1:766460161049:web:1cfc31a6fae29136f38e88"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
